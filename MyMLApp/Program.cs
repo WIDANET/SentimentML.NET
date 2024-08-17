12eefbd1e9e81498e02626c4b72613bc7b13887f
WIDANET <willianlopes162@gmail.com>
2024-08-17 17:24:55 -0300

    Adicionar arquivos de projeto.

---
+++ MyMLApp/Program.cs
@@ -0,0 +1,15 @@
+﻿using MyMLApp;
+
+
+Console.WriteLine("Vai zbct, digita alguma coisa aí:");
+var sampleData = new SentimentModel.ModelInput()
+{
+    Col0 = Console.ReadLine()
+};
+
+
+var result = SentimentModel.Predict(sampleData);
+
+
+var sentiment = result.PredictedLabel == 1 ? "Pica de cachorro" : "vsfd horrivel Kjkkkkkk";
+Console.WriteLine($"Texto: {sampleData.Col0}\nSentimento: {sentiment}");
